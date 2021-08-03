0c6803bea6e9be235ef5806d7775e2ab30880c65
kenwoo9y <ei.1px.9.01@gmail.com>
2021-08-03 23:32:02 +0900

    First commit

---
+++ src/main/java/com/example/gsspringboot/GsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSpringBootApplication.class, args);
+	}
+
+}
